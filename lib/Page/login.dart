76a49b822184a5282b8971843a0c62884b580505
hyungjinKim <hyungjin0105@naver.com>
2023-06-01 12:19:47 +0900

    second commit

--- lib/Page/login.dart
+++ lib/Page/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fit_palette2/Auth/Auth_service.dart';  // Add your path here
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,11 +9,55 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  AuthService _authService = AuthService();  // create an instance of AuthService
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            const SizedBox(height: 300.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/logo.png', height:50),
+                const SizedBox(height: 16.0),
+              ],
+            ),
+            const SizedBox(height: 120.0),
+            Container(
+              height: 70,
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () async {
+                  _authService.signInWithGoogle();  // use the instance to call the method
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      child: Image.asset('assets/google.png', height:50, fit: BoxFit.cover,)
+                    ),
+                    const SizedBox(width: 30),
+                    Flexible(
+                      child: Text("Sign in with Google",
+                        style: TextStyle(fontSize: 20,
+                        color: Colors.grey
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'Auth/Auth_service.dart';
 import 'firebase_options.dart';
+
+import 'package:fit_palette2/Auth/Auth_service.dart';
+import 'package:fit_palette2/Page/Home/MyWeight.dart';
+import 'package:fit_palette2/Page/Home/Home.dart';
+import 'package:fit_palette2/Page/Home/Profile.dart';
+import 'package:fit_palette2/Page/Home/Routine.dart';
+
+
+import 'package:fit_palette2/Page/Join/join1.dart';
+import 'package:fit_palette2/Page/Join/join2.dart';
+import 'package:fit_palette2/Page/Join/join3.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,8 +27,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes : { //route 설정
-      
+      routes : {
+        '/MyWeight': (context) => MyWeight(),
+        '/Profile': (context) => Profile(),
+        '/Routine' :(context) => Routine(),
+        '/Home' : (context) => Home(),
+        '/join1' : (context) => Join1Page(), 
+        '/join2' : (context) => Join2Page(), 
+        '/join3' : (context) => Join3Page() 
   },
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
